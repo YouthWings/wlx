72effba5f0991d7238599027ef1ae8fdb7ecfb70
lzj0726 <1622472966@qq.com>
2018-06-04 15:36:00 +0800

    1、更改了配置

--- gzh-biz/src/main/java/cn/dubidubi/dao/base/UserRoleMapper.java
+++ gzh-biz/src/main/java/cn/dubidubi/dao/base/UserRoleMapper.java
@@ -1,9 +1,8 @@
 package cn.dubidubi.dao.base;
 
-import cn.dubidubi.model.dto.AddRoleDTO;
+
 
 public interface UserRoleMapper {
 	Integer GetRoleIdByUserId(int id);
 
-	void addUserRole(AddRoleDTO addRoleDTO);
 }
--- gzh-biz/src/main/java/cn/dubidubi/service/impl/UserLoginServiceImpl.java
+++ gzh-biz/src/main/java/cn/dubidubi/service/impl/UserLoginServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-
 public class UserLoginServiceImpl implements UserLoginService {
     @Autowired
     UserMapper userMapper;
